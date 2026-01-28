904e588f8d3ec142cd321838e6ad5a93b087c112
Kiki1875b <kevinheo0413@gmail.com>
2026-01-28 18:01:22 +0900

    feat: add platform-specific reservation payloads for broker

---
+++ omnibook-broker/src/main/java/com/sprint/omnibook/omnibookcore/OmnibookCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.omnibook.omnibookcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnibookCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnibookCoreApplication.class, args);
+	}
+
+}
---
+++ omnibook-broker/src/test/java/com/sprint/omnibook/omnibookcore/OmnibookCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.omnibook.omnibookcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnibookCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
