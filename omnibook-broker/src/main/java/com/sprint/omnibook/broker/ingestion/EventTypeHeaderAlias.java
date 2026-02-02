fe1036c7acc17a0d13938b591636225c97c2aeb2
Kiki1875b <kevinheo0413@gmail.com>
2026-02-02 13:54:23 +0900

    refactor(broker): 이벤트 타입 헤더 및 에러 메시지 상수 클래스 추가

---
+++ omnibook-broker/src/main/java/com/sprint/omnibook/broker/ingestion/EventTypeHeaderAlias.java
@@ -0,0 +1,14 @@
+package com.sprint.omnibook.broker.ingestion;
+
+/**
+ * 이벤트 타입 헤더 매핑 상수.
+ */
+public final class EventTypeHeaderAlias {
+
+    public static final String BOOKING = "BOOKING";
+    public static final String CANCEL = "CANCEL";
+    public static final String CANCELLATION = "CANCELLATION";
+
+    private EventTypeHeaderAlias() {
+    }
+}
